9c2797ccb3a52f839c39e3d95d348fe1779730f6
JustOxy <mihhail.kononov@gmail.com>
2025-08-24 17:51:51 +0300

    gnssposget-server: added mutexes around state read/writes

--- aesd-gnssposget-server/gnssposget-server.c
+++ aesd-gnssposget-server/gnssposget-server.c
@@ -37,15 +37,15 @@ typedef struct
 typedef enum 
 {
     STATE_INIT = 0,
-    STATE_WAITING_FOR_CLIENT,
-    STATE_START_REQUESTED,
-    STATE_WORKING,
-    STATE_ABORT_REQUESTED,
-    STATE_FINISHED,
-    STATE_DONE,
-    STATE_ERROR,
-    STATE_UNEXPECTED_ERROR,
-    STATE_NUM_STATES = 8
+    STATE_WAITING_FOR_CLIENT = 1,
+    STATE_START_REQUESTED = 2,
+    STATE_WORKING = 3,
+    STATE_ABORT_REQUESTED = 4,
+    STATE_FINISHED = 5,
+    STATE_DONE = 6,
+    STATE_ERROR = 7,
+    STATE_UNEXPECTED_ERROR = 8,
+    STATE_NUM_STATES = 9
 } serverapp_states; 
 
 struct state_machine_params
@@ -65,7 +65,7 @@ struct state_machine_params
 pthread_t threads[NUM_THREADS];
 pthread_t listener_thread;
 task_params t_params[NUM_THREADS];
-pthread_mutex_t file_mutex;
+pthread_mutex_t state_mutex;
 
 Boolean teardown_requested = FALSE;
 Boolean status_requested = FALSE;
@@ -81,6 +81,8 @@ void server_run(task_params* arguments);
 void read_client_error(serverapp_states* current_state);
 Boolean send_to_client(int configured_fd, struct state_machine_params* sm_params, U8* buf);
 void teardown(void);
+void set_state(serverapp_states *state_var, serverapp_states new_state);
+serverapp_states get_state(serverapp_states *state_var);
 
 /* ---------------------------------------------  */
 /* Public functions */
@@ -93,6 +95,8 @@ void gnssposget_server_mainloop(int *listen_fd)
     
     while(1)
     {
+        pthread_mutex_init(&state_mutex, NULL);
+
         /* Accept incoming connection */
         conf_fd = socket_connections_accept_incoming(&client_addr, listen_fd);
         t_params[i].client_addr = &client_addr;
@@ -150,6 +154,7 @@ Boolean gnssposget_server_task(void* arg)
 void server_run(task_params* arguments)
 {
     Boolean is_running = TRUE;
+    serverapp_states cur_state;
     struct state_machine_params sm_params = {
         .in_buf = NULL,
         .conf_fd = arguments->conf_fd,
@@ -166,18 +171,18 @@ void server_run(task_params* arguments)
             break;
         }
 
-        switch (sm_params.current_state)
+        cur_state = get_state(&sm_params.current_state);
+        switch (cur_state)
         {
             case STATE_INIT:
             {
                 /* Start receiving messages */
                 printf("STATE_INIT\n");
-                printf("in_buf addr=0x%X\n", (unsigned int)sm_params.in_buf);
                 sm_params.run_listener = TRUE;
+                set_state(&sm_params.current_state, STATE_WAITING_FOR_CLIENT);
                 pthread_create(&listener_thread, NULL, (void*)listener_task, (void*)&sm_params);
-                sm_params.current_state = STATE_WAITING_FOR_CLIENT;
-            }
                 break;
+            }
             case STATE_WAITING_FOR_CLIENT:
             {
                 /* Listener is running. Just wait for a next state */
@@ -189,9 +194,10 @@ void server_run(task_params* arguments)
 
                 /* TODO: GNSS status received and it's good */
                 (void)send_to_client(arguments->conf_fd, &sm_params, "STATE_START_REQUESTED^WORKING^Signal=100percent\n");
-                sm_params.current_state = STATE_WORKING;
+                
+                set_state(&sm_params.current_state, STATE_WORKING);
+                break;
             }
-            break;
             case STATE_WORKING:
             {
                 /* TODO: Poll data from GNSS */
@@ -199,38 +205,49 @@ void server_run(task_params* arguments)
                 (void)send_to_client(arguments->conf_fd, &sm_params, "STATE_WORKING^RUNNING_STATUS^0#3.53\n");
                 (void)send_to_client(arguments->conf_fd, &sm_params, "STATE_WORKING^RUNNING_STATUS^1#6.79\n");
                 (void)send_to_client(arguments->conf_fd, &sm_params, "STATE_WORKING^RUNNING_TIMEOUT^2#30\n");
-                sm_params.current_state = STATE_DONE;
+                
+                set_state(&sm_params.current_state, STATE_DONE);
             }
             break;
             case STATE_ABORT_REQUESTED:
                 // Handle abort requested
                 break;
             case STATE_FINISHED:
+            {
                 // Handle finished
-                printf("STATE_FINISHED\n");
-                sm_params.current_state = STATE_INIT;
+                syslog(LOG_INFO, "STATE_FINISHED\n");
+                set_state(&sm_params.current_state, STATE_INIT);
+            }
                 break;
             case STATE_DONE:
+            {
                 // Handle done
                 syslog(LOG_INFO, "State done");
                 sm_params.run_listener = FALSE;
                 pthread_join(listener_thread, NULL);
-                sm_params.current_state = STATE_FINISHED;
+                set_state(&sm_params.current_state, STATE_FINISHED);
+            }
                 break;
             case STATE_ERROR:
+            {
                 // Handle error
                 teardown();
                 is_running = FALSE;
+            }
                 break;
             case STATE_UNEXPECTED_ERROR:
+            {
                 // Handle unexpected error
                 teardown();
                 is_running = FALSE;
+            }
                 break;
             default:
+            {
                 // Handle unknown state
                 teardown();
                 is_running = FALSE;
+            }
                 break;
         }
     }
@@ -239,6 +256,7 @@ void server_run(task_params* arguments)
 void listener_task(void* arg)
 {
     printf("Started listener thread\n");
+    serverapp_states cur_state;
     struct state_machine_params* params = (struct state_machine_params*)arg;
     while ((params->run_listener) == TRUE)
     {
@@ -246,6 +264,7 @@ void listener_task(void* arg)
                                                      CLIENT_RECEIVE_TIMEOUT,
                                                      &params->in_buf) == TRUE)
         {
+            cur_state = get_state(&params->current_state);
             if (strcmp(params->in_buf, "TIMEOUT") == 0)
             {
                 free(params->in_buf);
@@ -257,20 +276,21 @@ void listener_task(void* arg)
             {
                 free(params->in_buf);
                 params->in_buf = NULL;
-                if ((params->current_state == STATE_START_REQUESTED) ||
-                    (params->current_state == STATE_WORKING))
+                if ((cur_state == STATE_START_REQUESTED) ||
+                    (cur_state == STATE_WORKING))
                     {
                         syslog(LOG_INFO, "Received REQUEST_ABORT message");
                         /* TODO: Implement abort logic */
-                        params->current_state = STATE_DONE;
+                        set_state(&params->current_state, STATE_DONE);
                         params->run_listener = FALSE;
+                        (void)send_to_client(params->conf_fd, params, "ABORTED\n");
                     }
             }
             else if (strcmp(params->in_buf, "REQUEST_STATUS\n") == 0)
             {
                 free(params->in_buf);
                 params->in_buf = NULL;
-                if (params->current_state == STATE_WORKING)
+                if (cur_state == STATE_WORKING)
                 {
                     syslog(LOG_INFO, "Received REQUEST_STATUS message");
                     /* TODO: Implement getting status logic */
@@ -283,8 +303,7 @@ void listener_task(void* arg)
                 params->in_buf = NULL;
                 syslog(LOG_INFO, "Received STATE_INIT message");
                 /* TODO: Start getting GNSS here */
-
-                params->current_state = STATE_START_REQUESTED;
+                set_state(&params->current_state, STATE_START_REQUESTED);
             }
             else
             {
@@ -315,7 +334,7 @@ Boolean send_to_client(int configured_fd, struct state_machine_params* sm_params
         syslog(LOG_PERROR, "ERROR sending message to client");
         /* TODO: Kill everything GNSS related */
 
-        sm_params->current_state = STATE_UNEXPECTED_ERROR;
+        set_state(&sm_params->current_state, STATE_UNEXPECTED_ERROR);
         sm_params->run_listener = FALSE;
         return FALSE;
     }
@@ -323,12 +342,29 @@ Boolean send_to_client(int configured_fd, struct state_machine_params* sm_params
 
 void teardown(void)
 {
+    pthread_mutex_destroy(&state_mutex);
     pthread_join(listener_thread, NULL);
     for(int i = 0; i < NUM_THREADS; i++)
     {
         close(t_params[i].conf_fd);
-        //printClientIpAddress(FALSE, &t_params[i]);
         pthread_join(threads[i], NULL);
     }
 }
 
+void set_state(serverapp_states *state_var, serverapp_states new_state)
+{
+    pthread_mutex_lock(&state_mutex);
+    *state_var = new_state;
+    pthread_mutex_unlock(&state_mutex);
+}
+
+serverapp_states get_state(serverapp_states *state_var)
+{
+    serverapp_states current_state;
+
+    pthread_mutex_lock(&state_mutex);
+    current_state = *state_var;
+    pthread_mutex_unlock(&state_mutex);
+
+    return current_state;
+}
